b0e2512986272add5aad38226666b08fcb6193da
TisseraK <k.tisserand@outlook.fr>
2021-10-25 11:50:29 +0200

    Accueil + LogIn

--- lib/main.dart
+++ lib/main.dart
@@ -9,56 +9,22 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Nike Store',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+  MyHomePage({Key key});
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -68,46 +34,226 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(
+                'asset/image/fond_auth.jpeg'),
+            fit: BoxFit.fitHeight,
+          ),
+        ),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+          children: [
+Container(
+  margin: EdgeInsets.only(top: MediaQuery.of(context).size.height*0.1),
+  height: MediaQuery.of(context).size.height*0.15,
+  width: MediaQuery.of(context).size.width*0.5,
+  decoration: BoxDecoration(
+  image: DecorationImage(
+    image: AssetImage(
+        'asset/image/nike.png'),
+    fit: BoxFit.fitWidth,
+  ),
+),
+
+),
+            Container(
+              margin: EdgeInsets.only(top: MediaQuery.of(context).size.height*0.45),
+              height: MediaQuery.of(context).size.height*0.25,
+              width: MediaQuery.of(context).size.width*0.8,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  GestureDetector(
+                    onTap: (){
+                    },
+                    child: Container(
+                        padding: EdgeInsets.all(MediaQuery.of(context).size.height*0.01),
+                        height: MediaQuery.of(context).size.height*0.06,
+                        width: MediaQuery.of(context).size.width*0.6,
+                        decoration: BoxDecoration(
+                            border: Border.all(color: Color.fromARGB(200, 186, 186, 186),),
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                            color: Color.fromARGB(100, 0, 0, 0)
+                        ),
+                        child: Center(
+                          child: Text('Create an Account',style: TextStyle(color: Colors.grey[350],fontWeight: FontWeight.bold),
+                          ),)
+                    ),
+                  ) ,
+
+                  GestureDetector(
+                    onTap: (){
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => SignIn()),
+                      );
+                    },
+                    child:  Container(
+                        padding: EdgeInsets.all(MediaQuery.of(context).size.height*0.01),
+                        height: MediaQuery.of(context).size.height*0.06,
+                        width: MediaQuery.of(context).size.width*0.6,
+                        decoration: BoxDecoration(
+                            border: Border.all(color: Color.fromARGB(200, 186, 186, 186),),
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                            color: Color.fromARGB(100, 0, 0, 0)
+                        ),
+                        child: Center(
+                          child: Text('Log In',style: TextStyle(color: Colors.grey[350],fontWeight: FontWeight.bold),
+                          ),)
+                    ),
+                  ) ,
+                ],
+              ),
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      )
+    );
+  }
+}
+
+
+class SignIn extends StatefulWidget {
+  SignIn({Key key});
+  @override
+  SignInState createState() => SignInState();
+}
+
+class SignInState extends State<SignIn> {
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+        body: Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(
+                  'asset/image/fond_auth.jpeg'),
+              fit: BoxFit.fitHeight,
+            ),
+          ),
+          child: Column(
+            children: [
+              Container(
+                margin: EdgeInsets.only(top: MediaQuery.of(context).size.height*0.1),
+                height: MediaQuery.of(context).size.height*0.15,
+                width: MediaQuery.of(context).size.width*0.5,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage(
+                        'asset/image/nike.png'),
+                    fit: BoxFit.fitWidth,
+                  ),
+                ),
+
+              ),
+              Container(
+                margin: EdgeInsets.only(top: MediaQuery.of(context).size.height*0.1),
+                height: MediaQuery.of(context).size.height*0.25,
+                width: MediaQuery.of(context).size.width*0.8,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text('Email',textAlign: TextAlign.start,style: TextStyle(color:Colors.grey[350],shadows: <Shadow>[
+                          Shadow(
+                            offset: Offset(0.5, 0.5),
+                            blurRadius: 1.0,
+                            color: Color.fromARGB(150, 0, 0, 0),
+                          ),
+                        ],
+                        ),),
+                      ],
+                    ),
+                    Container(
+                      padding: EdgeInsets.all(MediaQuery.of(context).size.height*0.01),
+                      height: MediaQuery.of(context).size.height*0.06,
+                      width: MediaQuery.of(context).size.width*0.8,
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Color.fromARGB(250, 186, 186, 186),),
+                          borderRadius: BorderRadius.all(Radius.circular(10)),
+                          color: Color.fromARGB(150, 0, 0, 0)
+                      ),
+                      child: TextFormField(
+                        style: TextStyle(color: Colors.grey[350]),
+                        decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: 'Your email',
+                            hintStyle: TextStyle(color:Colors.grey[350] ),
+                            labelStyle: TextStyle(color:Colors.grey[350] )
+                        ),
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text('Password',textAlign: TextAlign.start,style: TextStyle(color:Colors.grey[350],shadows: <Shadow>[
+                          Shadow(
+                            offset: Offset(0.5, 0.5),
+                            blurRadius: 1.0,
+                            color: Color.fromARGB(150, 0, 0, 0),
+                          ),
+                        ],),),
+                      ],
+                    ),
+                    Container(
+                      padding: EdgeInsets.all(MediaQuery.of(context).size.height*0.01),
+                      height: MediaQuery.of(context).size.height*0.06,
+                      width: MediaQuery.of(context).size.width*0.8,
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Color.fromARGB(250, 186, 186, 186),),
+                          borderRadius: BorderRadius.all(Radius.circular(10)),
+                          color: Color.fromARGB(100, 0, 0, 0)
+                      ),
+                      child: TextFormField(
+                        style: TextStyle(color: Colors.grey[350]),
+                        decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: 'Your password',
+                            hintStyle: TextStyle(color:Colors.grey[350] ),
+                            labelStyle: TextStyle(color:Colors.grey[350] )
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: MediaQuery.of(context).size.height*0.2),
+                child:GestureDetector(
+                  onTap: (){
+                  },
+                  child: Container(
+                      padding: EdgeInsets.all(MediaQuery.of(context).size.height*0.01),
+                      height: MediaQuery.of(context).size.height*0.06,
+                      width: MediaQuery.of(context).size.width*0.6,
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Color.fromARGB(200, 186, 186, 186),),
+                          borderRadius: BorderRadius.all(Radius.circular(10)),
+                          color: Color.fromARGB(150, 0, 0, 0)
+                      ),
+                      child: Center(
+                        child: Text('Sign In',style: TextStyle(color: Colors.grey[350],fontWeight: FontWeight.bold),
+                        ),)
+                  ),
+                ) ,
+              )
+
+            ],
+          ),
+        )
     );
   }
 }
