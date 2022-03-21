ee7b4257e3881a76f54f6eee2b762f2bac0fb2a8
Tisserak <k.tisserand@outlook.fr>
2022-03-21 08:40:16 +0100

    Ajout de grid view pour les produits

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class Home extends StatefulWidget {
   Home({Key key});
   @override
@@ -10,104 +11,160 @@ class HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Container(
-          height: MediaQuery.of(context).size.height,
-          width: MediaQuery.of(context).size.width,
-          decoration: BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-                colors: [
-                  Color.fromARGB(255, 250, 250, 250),
-                  Color.fromARGB(255, 255, 255, 255),
-                  Color.fromARGB(255, 250, 250, 250),
-                ],
-              )
-          ),
-          child: Column(
-            children: [
-              Container(
-                height: MediaQuery.of(context).size.height*0.07,
-                width: MediaQuery.of(context).size.width*0.90,
-                child: Center(
-                    child:Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        GestureDetector(
-                          onTap: (){
-
-                          },
-                          child: Container(
-                            height: MediaQuery.of(context).size.height*0.035,
-                            width: MediaQuery.of(context).size.height*0.035,
-                            decoration: BoxDecoration(
-                              image: DecorationImage(
-                                image: AssetImage(
-                                    'asset/image/one.png'),
-                                fit: BoxFit.cover,
-                              ),
-                            ),
-                          ),
-                        ),
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceAround,
-                          children: [
-                          Text('Explorer', style: TextStyle(color: Color.fromARGB(255, 58, 99, 224),fontWeight: FontWeight.bold,fontSize:MediaQuery.of(context).size.height*0.03 ),),
-                        ],),
-                        GestureDetector(
-                          onTap: (){
-
-                          },
-                          child: Container(
-                            decoration: BoxDecoration(
-                              image: DecorationImage(
-                                image: AssetImage(
-                                    'asset/image/two.png'),
-                                fit: BoxFit.cover,
-                              ),
-                            ),
-                            height: MediaQuery.of(context).size.height*0.035,
-                            width: MediaQuery.of(context).size.height*0.035,
-                          ),
-                        ),
-                      ],
-                    )
-                ),
-
-              ),
-              Container(
-                height: MediaQuery.of(context).size.height*0.065,
-                width: MediaQuery.of(context).size.width*0.9,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(10),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.withOpacity(0.5),
-                      spreadRadius: 1,
-                      blurRadius: 1,
-                      offset: Offset(1, 1), // changes position of shadow
-                    ),
-                  ],
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    TextFormField(
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: "Recherche",
-                        hintStyle: TextStyle(fontSize:MediaQuery.of(context).size.height*0.02 ),labelStyle: TextStyle(fontSize:MediaQuery.of(context).size.height*0.02 ),
-
-                        prefixIcon: Icon(Icons.search),
-
+      padding: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.035),
+      height: MediaQuery.of(context).size.height,
+      width: MediaQuery.of(context).size.width,
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+        begin: Alignment.topCenter,
+        end: Alignment.bottomCenter,
+        colors: [
+          Color.fromARGB(255, 250, 250, 250),
+          Color.fromARGB(255, 255, 255, 255),
+          Color.fromARGB(255, 250, 250, 250),
+        ],
+      )),
+      child: Column(
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height * 0.07,
+            width: MediaQuery.of(context).size.width * 0.90,
+            child: Center(
+                child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                GestureDetector(
+                  onTap: () {},
+                  child: Container(
+                    height: MediaQuery.of(context).size.height * 0.035,
+                    width: MediaQuery.of(context).size.height * 0.035,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage('asset/image/one.png'),
+                        fit: BoxFit.cover,
                       ),
                     ),
+                  ),
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Text(
+                      'Explorer',
+                      style: TextStyle(
+                          color: Color.fromARGB(255, 58, 99, 224),
+                          fontWeight: FontWeight.bold,
+                          fontSize: MediaQuery.of(context).size.height * 0.03),
+                    ),
                   ],
-                )
-              )
-            ],
+                ),
+                GestureDetector(
+                  onTap: () {},
+                  child: Container(
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage('asset/image/two.png'),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    height: MediaQuery.of(context).size.height * 0.035,
+                    width: MediaQuery.of(context).size.height * 0.035,
+                  ),
+                ),
+              ],
+            )),
           ),
-        )
-    );
+          Container(
+              height: MediaQuery.of(context).size.height * 0.065,
+              width: MediaQuery.of(context).size.width * 0.9,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 1,
+                    blurRadius: 1,
+                    offset: Offset(1, 1), // changes position of shadow
+                  ),
+                ],
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: "Recherche",
+                      hintStyle: TextStyle(
+                          fontSize: MediaQuery.of(context).size.height * 0.02),
+                      labelStyle: TextStyle(
+                          fontSize: MediaQuery.of(context).size.height * 0.02),
+                      prefixIcon: Icon(Icons.search),
+                    ),
+                  ),
+                ],
+              )),
+          Container(
+            height: MediaQuery.of(context).size.height * 0.80,
+            width: MediaQuery.of(context).size.width,
+            child: GridView.count(
+              physics: ScrollPhysics(),
+              crossAxisCount: 2,
+              crossAxisSpacing: 2.0,
+              mainAxisSpacing: 2.0,
+              shrinkWrap: true,
+              scrollDirection: Axis.vertical,
+              padding: EdgeInsets.all(10),
+              children: [
+                Container(
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                            offset: Offset(3, 3),
+                            blurRadius: 0.5,
+                            color: Colors.grey.withOpacity(0.5))
+                      ],
+                      color: Colors.grey.shade200,
+                      borderRadius: BorderRadius.circular(30)),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Container(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: [
+                                Text('Epic React'),
+                                Text(
+                                  '192.00€',
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                )
+                              ],
+                            ),
+                          ),
+                          GestureDetector(
+                            child: Container(
+                              height: MediaQuery.of(context).size.height * 0.04,
+                              width: MediaQuery.of(context).size.height * 0.04,
+                              decoration: BoxDecoration(
+                                  color: Color.fromARGB(255, 58, 99, 224),
+                                  borderRadius: BorderRadius.circular(10)),
+                            ),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    ));
   }
 }
